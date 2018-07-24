0f864b0841c08d8a79430791d0b2599ee8efd9a9
冷冷 <wangiegie@gmail.com>
2018-07-24 18:47:36 +0800

    :bookmark: 发布一个版本。V 1.3.0

---
+++ pigx-visual/pigx-daemon/src/main/java/com/pig4cloud/pigx/daemon/PigxDaemonApplication.java
@@ -0,0 +1,37 @@
+/*
+ *    Copyright (c) 2018-2025, lengleng All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimer.
+ * Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ * Neither the name of the pig4cloud.com developer nor the names of its
+ * contributors may be used to endorse or promote products derived from
+ * this software without specific prior written permission.
+ * Author: lengleng (wangiegie@gmail.com)
+ */
+
+package com.pig4cloud.pigx.daemon;
+
+import com.pig4cloud.pigx.common.job.annotation.EnablePigxJob;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author lengleng
+ * @date 2018/7/24
+ * 分布式任务调度模块
+ */
+@EnablePigxJob
+@SpringBootApplication
+public class PigxDaemonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PigxDaemonApplication.class, args);
+	}
+
+}
--- pigx-visual/pigx-monitor/src/main/java/com/pig4cloud/pigx/monitor/PigxMonitorApplication.java
+++ pigx-visual/pigx-monitor/src/main/java/com/pig4cloud/pigx/monitor/PigxMonitorApplication.java
@@ -20,7 +20,6 @@
 package com.pig4cloud.pigx.monitor;
 
 
-import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
