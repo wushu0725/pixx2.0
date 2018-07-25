a0a874be9c16b537050502910fa0b2decfe0960e
冷冷 <wangiegie@gmail.com>
2018-07-25 09:58:08 +0800

    :recycle: 重构代码

--- pigx-eureka/src/main/java/com/pig4cloud/pigx/eureka/security/WebSecurityConfig.java
+++ pigx-eureka/src/main/java/com/pig4cloud/pigx/eureka/security/WebSecurityConfig.java
@@ -32,7 +32,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 		http.csrf().disable()
 			.authorizeRequests()
-			.antMatchers("actuator/**").permitAll()
+			.antMatchers("/actuator/**").permitAll()
 			.anyRequest()
 			.authenticated().and().httpBasic();
 	}
