9e6ca907bbf284793c353df4cd9f2d4fca350cc0
冷冷 <wangiegie@gmail.com>
2018-07-26 19:18:13 +0800

    :recycle: 重构代码。

--- pigx-common/pigx-common-core/src/main/java/com/pig4cloud/pigx/common/core/exception/ValidateCodeException.java
+++ pigx-common/pigx-common-core/src/main/java/com/pig4cloud/pigx/common/core/exception/ValidateCodeException.java
@@ -26,6 +26,9 @@ package com.pig4cloud.pigx.common.core.exception;
 public class ValidateCodeException extends Exception {
 	private static final long serialVersionUID = -7285211528095468156L;
 
+	public ValidateCodeException() {
+	}
+
 	public ValidateCodeException(String msg) {
 		super(msg);
 	}
