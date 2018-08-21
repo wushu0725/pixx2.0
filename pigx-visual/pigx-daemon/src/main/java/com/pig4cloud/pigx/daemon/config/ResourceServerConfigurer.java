d27a6ca35d0192645bad5a52c0d2db7183cc5a13
冷冷 <wangiegie@gmail.com>
2018-08-21 20:29:53 +0800

    :recycle: 重构代码。优化代码逻辑，方便扩展资源服务器

--- pigx-visual/pigx-daemon/src/main/java/com/pig4cloud/pigx/daemon/config/ResourceServerConfigurer.java
+++ pigx-visual/pigx-daemon/src/main/java/com/pig4cloud/pigx/daemon/config/ResourceServerConfigurer.java
@@ -17,15 +17,12 @@
 
 package com.pig4cloud.pigx.daemon.config;
 
-import com.pig4cloud.pigx.common.security.component.PigAccessDeniedHandler;
 import com.pig4cloud.pigx.common.security.component.PigxResourceServerConfigurerAdapter;
-import com.pig4cloud.pigx.common.security.component.ResourceAuthExceptionEntryPoint;
 import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
-import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
 
 /**
