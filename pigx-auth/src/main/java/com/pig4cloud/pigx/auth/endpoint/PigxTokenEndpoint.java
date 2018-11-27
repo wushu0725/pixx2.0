175d6601f5f3bc85cf4b64dca3decb0f07665c36
冷冷 <wangiegie@gmail.com>
2018-11-27 20:06:02 +0800

    :fire: Removing code or files.

--- pigx-auth/src/main/java/com/pig4cloud/pigx/auth/endpoint/PigxTokenEndpoint.java
+++ pigx-auth/src/main/java/com/pig4cloud/pigx/auth/endpoint/PigxTokenEndpoint.java
@@ -21,7 +21,6 @@ package com.pig4cloud.pigx.auth.endpoint;
 
 import cn.hutool.core.map.MapUtil;
 import cn.hutool.core.util.StrUtil;
-import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.pig4cloud.pigx.common.core.constant.PaginationConstant;
 import com.pig4cloud.pigx.common.core.constant.SecurityConstants;
