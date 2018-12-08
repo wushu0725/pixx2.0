23dbd28d4c73b95ebefe2b584283935da8821ebe
冷冷 <wangiegie@gmail.com>
2018-12-08 10:24:52 +0800

    :whale: Work about Docker.   完善脚本

--- doc/db/1schema.sql
+++ doc/db/1schema.sql
@@ -2,4 +2,4 @@
 create database `pigxx` default character set utf8mb4 collate utf8mb4_general_ci;
 
 -- pigx 工作流相关库
-create database `pigxx_acl` default character set utf8mb4 collate utf8mb4_general_ci;
+create database `pigxx_ac` default character set utf8mb4 collate utf8mb4_general_ci;
